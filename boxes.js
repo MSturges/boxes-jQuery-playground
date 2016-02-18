e8e960823d8381d9f04149911f0765c70ba2ae2e
MSturges <Sturges3232@gmail.com>
2016-02-17 20:56:42 -0700

    First attempt

--- boxes.js
+++ boxes.js
@@ -1,1 +1,89 @@
-console.log("hello world");
+// console.log("hello world");
+// $(document).ready(function() {
+//     alert( "The DOM is Ready" );
+// });
+//
+//
+// $(document ).ready(function(){
+//   var $secretBox = $('#secretBox')
+//   $secretBox.css('background-color', 'white');
+//
+// });
+//
+// $(document).ready(function() {
+//   $('#secretBox').append("<h1>secretBox!</h1>");
+// });
+//
+// $(document).ready(function(){
+//   $('#row1 > div').addClass('boxType3');
+// });
+//
+// $(document).ready(function(){
+//   $('#row4 div:last-child').css("display","none");
+// });
+//
+// $(document).ready(function(){
+//   $('.boxType1').css('background-color', 'white');
+// });
+
+// $(document).ready(function(){
+//   $('#row2 div:lt(2)').removeClass('box');
+// });
+
+// $(document).ready(function(){
+//   $( 'div' ).not( ".row, #secretBox")
+//     .css( "width", "2px" );
+// });
+
+
+// $(document).ready(function(){
+//   $('#container').on('click', function () {
+//       console.log(event.pageX, event.pageY);
+//   });
+// });
+
+// $(document).ready(function(){
+//   $( '.row div' ) .css( "width", "2px" );
+// });
+
+//Need Help on
+
+// $(document).ready(function(){
+//   $('.boxType1').append('<a href="http://www.galvanize.com/"">Galvanize</a>')
+// });
+//
+// $(document).ready(function(){
+//   $('a').on('click', function(){
+//     return alert(are you sure you want to leave?);
+//   });
+// });
+
+
+//For all box divs, add a click handler that adds an image of a cute puppy to the
+//box.  If the image is clicked again, remove the cute puppy.
+
+
+$(document).ready(function(){
+  $('.box').on('click' function(){
+    $('.box').()
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
